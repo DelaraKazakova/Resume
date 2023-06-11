902f2dda475f0a61ca1c2dbd966b96c5d7be5d44
Delara Kazakova <delarakazakova@gmail.com>
2023-06-11 14:19:28 +0300

    added language switch

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
+// Define an object to store the translations
+var translations = {};
+// Load the localization files dynamically
+function loadTranslations(lang) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'language/' + lang + '.json', true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      translations = JSON.parse(xhr.responseText);
+      updateContent(lang);
+    }
+  };
+  xhr.send();
+}
+// Update the content with translations
+function updateContent(lang) {
+  var elements = document.querySelectorAll('[data-translate]');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var translationKey = element.getAttribute('data-translate');
+    if (translationKey) {
+      if (translations[translationKey]) {
+        element.textContent = translations[translationKey];
+      } else {
+        // Fallback to default language (English)
+        element.textContent = translationKey;
+      }
+    }
+  }
+}
+// Trigger language switch when the dropdown changes
+var languageSelect = document.getElementById('languageSelect');
+languageSelect.addEventListener('change', function () {
+  var selectedLang = this.value;
+  loadTranslations(selectedLang);
+  document.documentElement.lang = selectedLang;
+});
+// Default language
+loadTranslations("en");
+
 
 if (window.innerWidth > 767) {
-
   const left = document.querySelectorAll(".text-end");
 
   const observer1 = new IntersectionObserver(entries => {
@@ -11,8 +50,8 @@ if (window.innerWidth > 767) {
       }
     });
   });
-  left.forEach(rowl => {
-    observer1.observe(rowl);
+  left.forEach(toLeft => {
+    observer1.observe(toLeft);
   });
 
   const right = document.querySelectorAll(".text-start");
@@ -25,7 +64,7 @@ if (window.innerWidth > 767) {
       }
     });
   });
-  right.forEach(rowr => {
-    observer2.observe(rowr);
+  right.forEach(toRight => {
+    observer2.observe(toRight);
   });
-} 
+}
